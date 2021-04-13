fff4eaba7a25b3ee02813219a58a87e901b59e31
longmen2019 <51213632+longmen2019@users.noreply.github.com>
2021-04-13 17:21:11 -0400

    Add files via upload

---
+++ Create a Data Frame Using Pandas.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 13 17:17:25 2021
+
+@author: User
+"""
+
+#Create a DataFrame using Pandas to capture the dataset
+
+import pandas as pd
+
+data = {
+        'A': [45, 37 , 42, 35 , 39],
+        'B': [38, 31 , 26, 28 , 33],
+        'C': [10, 15 , 17, 21 , 12]
+        }
+df = pd.DataFrame(data, columns = ['A' , 'B' , 'C'])
+print(df)
